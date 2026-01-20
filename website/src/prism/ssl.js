de755743e020aaadea55e2f88eb4921b5a718830
Jan Šimek <jsimek.cz@gmail.com>
2026-01-20 22:12:46 +0100

    Fix SSL syntax highlighting

--- website/src/prism/ssl.js
+++ website/src/prism/ssl.js
@@ -1,6 +1,9 @@
 /**
  * Prism syntax highlighting for SSL (Star Trek Scripting Language)
  * Used in Fallout 2 modding.
+ *
+ * This is the single source of truth for SSL language definition.
+ * Imported by prism-include-languages.ts for Docusaurus.
  */
 
 const sslLanguage = {
@@ -22,46 +25,31 @@ const sslLanguage = {
     pattern: /#\s*(?:include|define|ifdef|ifndef|else|endif|undef)\b.*/,
     alias: 'property',
   },
-  keyword: [
-    {
-      // Control flow
-      pattern:
-        /\b(?:if|then|else|while|do|begin|end|return|call|procedure|variable|for|foreach|in|break|continue)\b/,
-      alias: 'keyword',
-    },
-    {
-      // Logical operators
-      pattern: /\b(?:and|or|not|bwand|bwor|bwxor|bwnot)\b/,
-      alias: 'keyword',
-    },
-  ],
+  keyword:
+    /\b(?:if|then|else|while|do|begin|end|return|call|procedure|variable|for|foreach|in|break|continue|and|or|not|bwand|bwor|bwxor|bwnot|import|export)\b/,
   'class-name': {
     pattern: /\b(?:int|void|float|string|boolean|ObjectPtr|any)\b/,
-    alias: 'type',
+    alias: 'class-name',
   },
   builtin: {
-    // Common SSL built-in functions
     pattern:
-      /\b(?:self_obj|dude_obj|source_obj|target_obj|obj_pid|obj_name|obj_carrying_pid_obj|tile_contains_obj_pid|critter_stat|set_critter_stat|critter_heal|critter_dmg|critter_add_trait|critter_rm_trait|critter_state|critter_kill_type|critter_injure|critter_uninjure|has_skill|using_skill|roll_vs_skill|skill_contest|is_success|is_critical|how_much|random|roll_dice|move_to|create_object|destroy_object|tile_num|elevation|tile_distance|tile_distance_objs|cur_map_index|display_msg|debug_msg|float_msg|message_str|gsay_start|gsay_end|gsay_reply|gsay_option|gsay_message|start_gdialog|end_dialogue|play_sfx|play_gmovie|game_time|game_time_hour|game_ticks|add_timer_event|rm_timer_event|script_overrides|get_local_var|set_local_var|get_map_var|set_map_var|get_global_var|set_global_var|global_var|local_var|map_var|give_xp|add_obj_to_inven|rm_obj_from_inven|inven_ptr|item_caps_total|item_caps_adjust|wield_obj_critter|use_obj|obj_item_subtype|combat_is_initialized|attack|terminate_combat|animate_stand|animate_move_obj_to_tile|reg_anim_func|reg_anim_animate|reg_anim_animate_reverse|reg_anim_obj_move_to_obj|reg_anim_obj_run_to_obj|reg_anim_obj_move_to_tile|reg_anim_obj_run_to_tile|reg_anim_play_sfx|reg_anim_animate_forever|anim_busy|metarule|metarule3|set_light_level|obj_set_light_level|fade_in|fade_out|load_map|mark_area_known|obj_is_visible|obj_is_locked|obj_lock|obj_unlock|obj_open|obj_close|obj_on_screen|obj_art_fid|art_anim|explosion|fire_at|critter_attempt_placement|critter_add_aid|run_dialog_reaction|party_add|party_remove|party_member_count|rotation_to_tile|tile_num_in_direction|tile_in_tile_rect|obj_type|obj_item_subtype|get_proto_data|set_proto_data|get_critter_current_ap|set_critter_current_ap|weapon_anim_code|weapon_damage_type)\b/,
+      /\b(?:action_being_used|add_mult_objs_to_inven|add_obj_to_inven|add_timer_event|anim|anim_action_frame|anim_busy|animate_move_obj_to_tile|animate_rotation|animate_run_to_tile|animate_set_frame|animate_stand|animate_stand_obj|animate_stand_reverse|animate_stand_reverse_obj|art_anim|attack|attack_complex|attack_setup|car_current_town|car_give_gas|car_give_to_party|combat_difficulty|combat_is_initialized|create_object|create_object_sid|critter_add_trait|critter_attempt_placement|critter_dmg|critter_heal|critter_injure|critter_inven_obj|critter_is_fleeing|critter_mod_skill|critter_rm_trait|critter_set_flee_state|critter_skill_level|critter_state|critter_stop_attacking|cur_map_index|cur_town|days_since_visited|debug_msg|destroy_mult_objs|destroy_object|dialogue_reaction|dialogue_system_enter|difficulty_level|display_msg|do_check|drop_obj|drug_influence|dude_obj|elevation|end_dialogue|endgame_movie|endgame_slideshow|explosion|fixed_param|float_msg|game_ticks|game_time|game_time_advance|game_time_hour|game_ui_disable|game_ui_enable|game_ui_is_disabled|gdialog_barter|gdialog_mod_barter|gdialog_set_barter_mod|get_critter_stat|get_day|get_month|get_pc_stat|get_poison|gfade_in|gfade_out|giQ_Option|give_exp_points|global_var|goto_xy|gSay_End|gSay_Message|gSay_Option|gSay_Reply|gSay_Start|has_skill|has_trait|how_much|inven_cmds|inven_count|inven_ptr|inven_unwield|is_critical|is_loading_game|is_skill_tagged|is_success|item_caps_adjust|item_caps_total|jam_lock|kill_critter|kill_critter_type|language_filter_is_on|load_map|local_var|map_first_run|map_is_known|map_known|map_var|message_str|move_obj_inven_to_obj|move_to|obj_art_fid|obj_being_used_with|obj_can_hear_obj|obj_can_see_obj|obj_carrying_pid_obj|obj_close|obj_drop_everything|obj_is_carrying_obj_pid|obj_is_locked|obj_is_open|obj_is_visible_flag|obj_item_subtype|obj_lock|obj_name|obj_on_screen|obj_open|obj_pid|obj_set_light_level|obj_type|obj_unlock|override_map_start|party_add|party_member_count|party_member_obj|party_remove|pickup_obj|play_gmovie|play_sfx|poison|proto_data|radiation_dec|radiation_inc|random|reg_anim_animate|reg_anim_animate_forever|reg_anim_animate_reverse|reg_anim_begin|reg_anim_clear|reg_anim_end|reg_anim_obj_move_to_obj|reg_anim_obj_move_to_tile|reg_anim_obj_run_to_obj|reg_anim_obj_run_to_tile|reg_anim_play_sfx|rm_fixed_timer_event|rm_mult_objs_from_inven|rm_obj_from_inven|rm_timer_event|roll_dice|roll_vs_skill|rotation_to_tile|running_burning_guy|scr_return|script_action|script_overrides|self_obj|set_critter_stat|set_exit_grids|set_global_var|set_light_level|set_local_var|set_map_start|set_map_var|set_obj_visibility|signal_end_game|skill_contest|source_obj|start_dialogue|start_gdialog|start_gialog|target_obj|terminate_combat|tile_contains_obj_pid|tile_contains_pid_obj|tile_distance|tile_distance_objs|tile_is_visible|tile_num|tile_num_in_direction|town_known|town_map|use_obj|use_obj_on_obj|using_skill|violence_level_setting|wield_obj|wield_obj_critter|wm_area_set_pos|world_map|world_map_x_pos|world_map_y_pos)\b/,
     alias: 'function',
   },
   constant: {
-    // Common constants
     pattern:
-      /\b(?:STAT_[A-Za-z_]+|SKILL_[A-Za-z_]+|PERK_[A-Za-z_]+|TRAIT_[A-Za-z_]+|PID_[A-Za-z_]+|SCRIPT_[A-Za-z_]+|OBJ_TYPE_[A-Za-z_]+|ITEM_TYPE_[A-Za-z_]+|SCENERY_TYPE_[A-Za-z_]+|DAMAGE_TYPE_[A-Za-z_]+|DMG_[A-Za-z_]+|KILL_TYPE_[A-Za-z_]+|GVAR_[A-Za-z_]+|LVAR_[A-Za-z_]+|MVAR_[A-Za-z_]+|FLOAT_MSG_[A-Za-z_]+|INVEN_TYPE_[A-Za-z_]+|GOOD|NEUTRAL|BAD|DAM_[A-Za-z_]+|TRUE|FALSE|true|false)\b/,
+      /\b(?:STAT_[A-Za-z_]+|SKILL_[A-Za-z_]+|PERK_[A-Za-z_]+|TRAIT_[A-Za-z_]+|PID_[A-Za-z_]+|SCRIPT_[A-Za-z_]+|OBJ_TYPE_[A-Za-z_]+|ITEM_TYPE_[A-Za-z_]+|DAMAGE_TYPE_[A-Za-z_]+|DMG_[A-Za-z_]+|KILL_TYPE_[A-Za-z_]+|GVAR_[A-Za-z_]+|LVAR_[A-Za-z_]+|MVAR_[A-Za-z_]+|FLOAT_MSG_[A-Za-z_]+|INVEN_TYPE_[A-Za-z_]+|INVEN_CMD_[A-Za-z_]+|ANIM_[A-Za-z_]+|DAM_[A-Za-z_]+|PCSTAT_[A-Za-z_]+|METARULE[0-9]*_[A-Za-z_]+|GOOD|NEUTRAL|BAD|TRUE|FALSE|true|false|NULL)\b/,
     alias: 'constant',
   },
   number: [
     {
-      // Hexadecimal
       pattern: /\b0x[0-9a-fA-F]+\b/,
     },
     {
-      // Decimal and float
       pattern: /\b\d+(?:\.\d+)?\b/,
     },
   ],
-  operator: /\+|\-|\*|\/|%|:=|==|!=|<=|>=|<|>/,
+  operator: /\+|-|\*|\/|%|:=|==|!=|<=|>=|<|>/,
   punctuation: /[{}[\];(),]/,
 };
 
